1e62698f34d1f04c606d4c7007d2b04d844e91ca
Micah Poe <micahpoe@gmail.com>
2023-03-27 02:44:11 +0000

    functions

--- inputOutput/greeter.js
+++ inputOutput/greeter.js
@@ -1,4 +1,9 @@
-let rlSync = require('readline-sync');
-let firstName = rlSync.question("What is your first name?\n");
-let lastName = rlSync.question("What is your last name?\n");
+function getName(prompt) {
+let readlineSync = require('readline-sync');
+let name = readlineSync.question(prompt);
+return name;
+}
+
+let firstName = getName('What is your first name? ');
+let lastName = getName('What is your last name? ');
 console.log(`Hello, ${firstName} ${lastName}!`);
